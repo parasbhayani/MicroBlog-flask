1a4a3f582fa060bf4a62ddf094bd78d65ac752de
Paras Bhayani <paras.byn@gmail.com>
2020-07-17 01:21:14 +0530

    Logging to a File using RotatingFileHandler

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -24,20 +25,20 @@ if not app.debug:
 		if app.config['MAIL_USE_TLS']:
 			secure = ()
 		mail_handler = SMTPHandler(
-			mailhost=(app.config['MAIL_SERVER], app.config['MAIL_PORT']),
+			mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
 			fromaddr='no-reply@' + app.config['MAIL_SERVER'],
 			toaddrs=app.config['ADMINS'], subject='Microblog Failure',
 			credentials=auth, secure=secure)
-		mail_handler.setlevel(logging.ERROR)
-		app.logger.addhandler(mail_handler)
+		mail_handler.setLevel(logging.ERROR)
+		app.logger.addHandler(mail_handler)
 	
 	if not os.path.exists('logs'):
 		os.mkdir('logs')
-	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount-10)
+	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
 	file_handler.setFormatter(logging.Formatter(
 		'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
 	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_hanlder)
+	app.logger.addHandler(file_handler)
 	
 	app.logger.setLevel(logging.INFO)
 	app.logger.info('Microblog startup')
