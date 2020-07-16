ca090cd9ee62135d765a15ff54ed5bc4f638a3d1
Paras Bhayani <paras.byn@gmail.com>
2020-07-17 01:11:14 +0530

    Logging to a File using RotatingFileHandler

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -30,3 +30,14 @@ if not app.debug:
 			credentials=auth, secure=secure)
 		mail_handler.setlevel(logging.ERROR)
 		app.logger.addhandler(mail_handler)
+	
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount-10)
+	file_handler.setFormatter(logging.Formatter(
+		'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_hanlder)
+	
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('Microblog startup')
